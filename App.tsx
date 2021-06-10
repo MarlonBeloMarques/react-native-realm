2f5a877f192aade01f7f4b2011843015a9aec5d1
MarlonMarques <marlon.belohd@gmail.com>
2021-06-10 15:39:04 -0400

    adiciona formulario
    adiciona formulario
    
    adiciona formulario

--- App.tsx
+++ App.tsx
@@ -8,29 +8,77 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import LinearGradient from 'react-native-linear-gradient';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
+    <LinearGradient
+      colors={['#7159c1', '#9B49c1']}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
+      <Text style={styles.title}>Repositórios</Text>
+      <View style={styles.view}>
+        <TextInput
+          placeholder="Procurar repositório..."
+          style={styles.input}
+          placeholderTextColor={'#999'}
         />
-      </ScrollView>
-    </SafeAreaView>
+        <TouchableOpacity style={styles.add} activeOpacity={0.8}>
+          <Icon name="add" size={22} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30 + getStatusBarHeight(true),
+  },
+  title: {
+    fontSize: 32,
+    color: '#FFF',
+    fontWeight: 'bold',
+    paddingLeft: 20,
+  },
+  view: {
+    flexDirection: 'row',
+    marginTop: 10,
+    paddingTop: 0,
+    paddingHorizontal: 20,
+  },
+  input: {
+    flex: 1,
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 15,
+    borderRadius: 4,
+    fontSize: 16,
+    color: '#333',
+    backgroundColor: '#FFF',
+  },
+  add: {
+    backgroundColor: '#6BD4C1',
+    marginLeft: 10,
+    justifyContent: 'center',
+    borderRadius: 4,
+    paddingHorizontal: 10,
+  },
+});
 
 export default App;
